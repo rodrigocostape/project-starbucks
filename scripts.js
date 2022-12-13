8efa4cb87faf36f0f80be03a9b693583502aa782
rodrigocostape~ <rc.pe93@gmail.com~>
2022-12-13 00:03:42 -0300

    starbucks

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+
+function trocaimagem(endereco){
+    imagem.src = endereco
+}
+
+function trocacor(cor){
+   circulo.style.background = cor
+
+}     
